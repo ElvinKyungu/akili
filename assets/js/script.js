b9f7da9428f8200d1993729db68d8a59ab2f4699
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-03-21 09:46:44 +0100

    sous mon pc, premier commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,6 @@
 
 
 
-// notification toast variables
-const notificationToast = document.querySelector('[data-toast]');
-const toastCloseBtn = document.querySelector('[data-toast-close]');
-
-// notification toast eventListener
-toastCloseBtn.addEventListener('click', function () {
-  notificationToast.classList.add('closed');
-});
-
-
-
 
 
 // mobile menu variables
